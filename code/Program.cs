108b8129f8c55c31b9748f1bf33bd2c4a2e3a771
Dmitriy Demakin <110729090+d-semakin@users.noreply.github.com>
2023-11-13 00:19:21 +0300

    Create Program.cs

---
+++ code/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void PrintArray(string[] array)
+    {
+        int leng = array.Length;
+        for (int i = 0; i < leng; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        string[] array = { "Hello", "2", "world", ":-)" };
+        //string[] array = {"1234", "1567", "-2", "computer science"};
+        //string[] array = {"Russia", "Denmark", "Kazan"};
+        int wordlength = 3;
+        int k = 0;
+        int leng = array.Length;
+        string[] arr = new string[leng];
+
+        for (int i = 0; i < leng; i++)
+        {
+            if (array[i].Length <= wordlength)
+            {
+                arr[i] = array[i];
+                k++;
+            }
+        }
+
+        Console.WriteLine("Исходный массив");
+        PrintArray(array);
+
+        Console.WriteLine("Итоговый массив");
+        if (k != 0) PrintArray(arr);
+        else Console.WriteLine("array is null");
+    }
+}
